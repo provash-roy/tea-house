fb9ea7391ad5b2cdc34e8f5a76b8a5bb6e997fbb
Provash-roy <provashray94@gmail.com>
2026-02-17 21:31:40 +0600

    grid unequal part practice

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="grid m-4 gap-4 sm:grid-cols-12">
+      <div className="bg-amber-500 min-h-[100px] rounded shadow-lg sm:col-span-2"></div>
+      <div className="bg-teal-500 min-h-[100px] rounded shadow-lg sm:col-span-8"></div>
+      <div className="bg-red-500 min-h-[100px] rounded shadow-lg sm:col-span-2"></div>
+    </div>
+  );
+}
+
+export default App;
